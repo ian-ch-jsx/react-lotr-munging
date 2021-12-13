c2f8dc91c828f4c07f2b2ba1c7b24bf417a9b794
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-13 14:45:49 -0800

    film data displaying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 import CharacterList from './components/Characters/CharacterList';
 import FilmList from './components/Films/FilmList';
@@ -21,36 +21,23 @@ function App() {
       },
     });
     const data = await resp.json();
-    console.log(data);
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
-    // 2. Transform the response so that films contains nested arrays of:
-    //   - the film's title
-    //   - the film's title "slugified" i.e. in all lower case, with words separated with dashes,
-    //   - the box office total
-    //   - academy award nominations
-    // NOTE: make sure you look at the response from the server - it may not be consistent
-    // [["The Lord of the Rings Series", "the-lord-of-the-rings-series", 2917, 30 ], ["The Hobbit Series", "the-hobit-series", 2932, 7]...]
+    const filmList = data.docs;
 
-    // 3. Set the resulting transformation as state using setFilms
-    // 4. You'll know it works if the films show up on the page
-
+    const filmArray = filmList.map((film) => {
+      return [film.name, film.name, film.boxOfficeRevenueInMillions, film.academyAwardNominations];
+    });
+    setFilms(filmArray);
     return [];
   };
 
   const getCharacters = async () => {
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/character/
-    // 2. Update the response data with the key `dates` which is a combination of
-    //    the `birth` key and the `death key` separated with a dash. If neither date
-    //    is provided, it should hold the string 'Unknown'
-    //    [
-    //       {name: 'Adanel', birth: "", death: "", dates: "Unknown", ...},
-    //       {name: 'Adrahil I', birth: "Before , TA 1944", death: "Late , Third Age", dates: "Before , TA 1944 - Late , Third Age", ...},
-    //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
-    //    ]
-    // 3. Set the resulting transformation as state using setCharacters
-    // 4. You'll know it works if the characters show up on the page
+    const resp = await fetch('https://the-one-api.dev/v2/character/', {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+    const data = await resp.json();
+
     return [];
   };
 
@@ -65,6 +52,11 @@ function App() {
             Characters
           </NavLink>
         </header>
+        <Switch>
+          <Route path="/films">
+            <FilmList films={films}></FilmList>
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
